a8ec74ba6bb627ffa8f2b6f5e640271eea6a1f3c
mark <mark@mark@kyin.org>
2019-02-01 07:53:00 -0500

    Start New Project

---
+++ src/bbbgpiobb.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : bbbgpiobb.c
+ Author      : Mark Meadows
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("TCPMux"); /* prints TCPMux */
+	return EXIT_SUCCESS;
+}
