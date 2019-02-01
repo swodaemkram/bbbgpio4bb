79582dcc6091e7812d0b5690555b54b2c6e3140a
mark <mark@mark@kyin.org>
2019-02-01 07:58:18 -0500

    2nd Commit Lets Get it popin

--- src/bbbgpiobb.c
+++ src/bbbgpiobb.c
@@ -2,16 +2,44 @@
  ============================================================================
  Name        : bbbgpiobb.c
  Author      : Mark Meadows
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style a
+ Version     : 00.00.01
+ Copyright   : copyright Fireking Security Group
+ Description : GPIO Driver for the BeagelBone Black for BlackBox
  ============================================================================
  */
 
+#include <unistd.h>
 #include <stdio.h>
-#include <stdlib.h>
 
-int main(void) {
-	puts("TCPMux"); /* prints TCPMux */
-	return EXIT_SUCCESS;
+
+int main()
+{
+   FILE *export_file = NULL; //declare pointers
+   FILE *IO_direction = NULL;
+   FILE *IO_value = NULL;
+   char str1[] = "0";
+   char str2[] = "1";
+   char str3[] = "out";
+   char str[] = "23";
+   //this part here exports gpio23
+   export_file = fopen ("/sys/class/gpio/export", "w");
+   fwrite (str, 1, sizeof(str), export_file);
+   fclose (export_file);
+   //this part here sets the direction of the pin
+   IO_direction = fopen("/sys/class/gpio/gpio23/direction", "w");
+   fwrite(str3, 1, sizeof(str3), IO_direction); //set the pin to HIGH
+   fclose(IO_direction);
+   usleep (1000000);
+
+   for (int i=0; i<10; i++){ //blink LED 10 times
+        IO_value = fopen ("/sys/class/gpio/gpio23/value", "w");
+        fwrite (str2, 1, sizeof(str2), IO_value); //set the pin to HIGH
+        fclose (IO_value);
+        usleep (1000000); //delay for a second
+
+        IO_value = fopen ("/sys/class/gpio/gpio23/value", "w");
+        fwrite (str1, 1, sizeof(str1), IO_value); //set the pin to LOW
+        fclose (IO_value);
+        usleep (1000000); //delay for a second
+        }
 }
