f38bcc8a4fe31f04637f74b0648748be1cb5fd6b
mark <mark@mark@kyin.org>
2019-02-04 13:44:11 -0500

    Next Step

--- src/bbbgpiobb.c
+++ src/bbbgpiobb.c
@@ -1,46 +1,46 @@
 /*
- ============================================================================
- Name        : bbbgpiobb.c
- Author      : Mark Meadows
- Version     : 00.00.01
- Copyright   : copyright Fireking Security Group
- Description : GPIO Driver for the BeagelBone Black for BlackBox
-               using BlackBox APIs
- ============================================================================
- */
+===========================================================================================================================
+Name		:	bbbgpiobb.c
+Author		:	Mark Meadows
+Version		:	v 0.0.1
+Copyright	:	Fireking Security Group
+Description	:	Service to provide GPIO In and Out capability to BlackBox on the BeagelBone Hardware Platform
+===========================================================================================================================
+*/
 
 #include <unistd.h>
 #include <stdio.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+
+#define VER "v 0.0.01\n"
 
 
-int main()
+#include "user_led2_flash.h"
+
+
+
+
+int main(int argc, char *argv[])
 {
-   FILE *export_file = NULL; //declare pointers
-   FILE *IO_direction = NULL;
-   FILE *IO_value = NULL;
-   char str1[] = "0";
-   char str2[] = "1";
-   char str3[] = "out";
-   char str[] = "23";
-   //this part here exports gpio23
-   export_file = fopen ("/sys/class/gpio/export", "w");
-   fwrite (str, 1, sizeof(str), export_file);
-   fclose (export_file);
-   //this part here sets the direction of the pin
-   IO_direction = fopen("/sys/class/gpio/gpio23/direction", "w");
-   fwrite(str3, 1, sizeof(str3), IO_direction); //set the pin to HIGH
-   fclose(IO_direction);
-   usleep (1000000);
 
-   for (int i=0; i<10; i++){ //blink LED 10 times
-        IO_value = fopen ("/sys/class/gpio/gpio23/value", "w");
-        fwrite (str2, 1, sizeof(str2), IO_value); //set the pin to HIGH
-        fclose (IO_value);
-        usleep (1000000); //delay for a second
 
-        IO_value = fopen ("/sys/class/gpio/gpio23/value", "w");
-        fwrite (str1, 1, sizeof(str1), IO_value); //set the pin to LOW
-        fclose (IO_value);
-        usleep (1000000); //delay for a second
-        }
+
+
+user_led2_flash(); //flash user LED to show service is running
+
+
+
+
+
+
+
+
+
+
+
+
+return(0);
 }
---
+++ src/user_led2_flash.h
@@ -0,0 +1,33 @@
+/*
+ * user_led2.h
+ *
+ *  Created on: Feb 4, 2019
+ *      Author: mark
+ */
+
+#ifndef USER_LED2_H_
+#define USER_LED2_H_
+
+
+
+#endif /* USER_LED2_H_ */
+
+#include <unistd.h>
+#include <stdio.h>
+
+void user_led2_flash(void){
+
+
+FILE *user_led2 = NULL; //declare User LED Pointer
+
+user_led2 = fopen("/sys/devices/platform/leds/leds/beaglebone:green:usr2/brightness", "w");
+fwrite("1",1,sizeof("1"),user_led2);
+fclose(user_led2);
+
+sleep(1);
+
+user_led2 = fopen("/sys/devices/platform/leds/leds/beaglebone:green:usr2/brightness", "w");
+fwrite("0",1,sizeof("0"),user_led2);
+fclose(user_led2);
+
+}
