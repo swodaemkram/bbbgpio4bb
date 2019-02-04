1f2a79cb0290b2cd76e7cf12a9c333c27fcc29e6
mark <mark@mark@kyin.org>
2019-02-04 14:39:44 -0500

    Help File Completed

---
+++ src/Print_Help.h
@@ -0,0 +1,27 @@
+/*
+ * Print_Help.h
+ *
+ *  Created on: Feb 4, 2019
+ *      Author: mark
+ */
+
+#ifndef PRINT_HELP_H_
+#define PRINT_HELP_H_
+
+#include <unistd.h>
+#include <stdio.h>
+
+void Print_Help(void){
+
+printf("\n  bbbgpiobb By Mark Meadows v 00.00.01\n");
+printf("          copyright Fireking 2019\n");
+printf(" a command line BlackBox interface to the BagelBone I/O hardware\n");
+printf(" -A, The I.P. Address for out-going BlackBox communications\n");
+printf(" -P, The Port for out-going BlackBox communications\n");
+printf(" -p, The Port used for in-coming BlackBox communications\n");
+printf("\n Example usage bbbgpiobb -A 192.168.1.15 -P 12000 -p 8080 \n " );
+
+exit(0);
+}
+
+#endif /* PRINT_HELP_H_ */
--- src/bbbgpiobb.c
+++ src/bbbgpiobb.c
@@ -15,32 +15,77 @@ Description	:	Service to provide GPIO In and Out capability to BlackBox on the B
 #include <sys/socket.h>
 #include <stdlib.h>
 
-#define VER "v 0.0.01\n"
-
 
 #include "user_led2_flash.h"
+#include "Print_Help.h"
 
+int main(int argc, char *argv[]){
 
+char IP_Out_To_BlackBox[15] = {0};
+int Port_Out_To_BlackBox = 0 ;
+int Port_IN_From_BlackBox = 0 ;
 
+/*
+==========================================================================================================================
+Load Command line arguments
+==========================================================================================================================
+ */
 
-int main(int argc, char *argv[])
-{
+	int z;
+	for (z=1; z < argc; z++)
+		{
+			if (argv[z][0] == '-')
 
+			{
+				switch (argv[z][1])
 
+				{
 
+				case 'A' :
+
+					strcpy(IP_Out_To_BlackBox, argv[z+1]);
+					break;
+
+				case 'P':
+
+					Port_Out_To_BlackBox = atoi(argv[z+1]);
+					break;
+
+				case 'p' :
+
+					Port_IN_From_BlackBox = atoi(argv[z+1]);
+					break;
+
+				case 'h' :
+				   Print_Help();
+					break;
+
+				case 'H' :
+				   Print_Help();
+				    break;
+
+ 				case 'v' :
+ 				   Print_Help();
+					break;
+
+ 				case 'V' :
+ 				   Print_Help();
+ 				    break;
+				}
+
+			}
+		}
+/*
+======================================================================================================================
+Command line arguments Loaded
+======================================================================================================================
+*/
 
 user_led2_flash(); //flash user LED to show service is running
 
-
-
-
-
-
-
-
-
-
-
+printf("\n IP_out_To_BlackBox = %s\n", IP_Out_To_BlackBox);
+printf("\n Port_Out_To_BlackBox = %d\n", Port_Out_To_BlackBox );
+printf("\n Port_IN_From_BlackBox = %d\n\n", Port_IN_From_BlackBox );
 
 return(0);
 }
