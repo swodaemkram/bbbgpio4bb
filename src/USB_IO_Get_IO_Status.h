7c5f435a32333e2f97459ccf1b8de825a4bbd7ce
mark <mark@mark@kyin.org>
2019-02-13 08:43:52 -0500

    Started Implementing USBIO

---
+++ src/USB_IO_Get_IO_Status.h
@@ -0,0 +1,18 @@
+/*
+ * USB_IO_Get_IO_Status.h
+ *
+ *  Created on: Feb 13, 2019
+ *      Author: mark
+ */
+
+#ifndef USB_IO_GET_IO_STATUS_H_
+#define USB_IO_GET_IO_STATUS_H_
+
+
+
+#endif /* USB_IO_GET_IO_STATUS_H_ */
+
+char *USB_IO_Get_IO_Status(void){
+	printf("\nUSBIO Not Implemented Yet....\n");
+	exit(0);
+}
---
+++ src/USB_IO_Initialize.h
@@ -0,0 +1,18 @@
+/*
+ * USB_IO_Initialize.h
+ *
+ *  Created on: Feb 13, 2019
+ *      Author: mark
+ */
+
+#ifndef USB_IO_INITIALIZE_H_
+#define USB_IO_INITIALIZE_H_
+
+
+
+#endif /* USB_IO_INITIALIZE_H_ */
+
+void USB_IO_Initialize(void){
+	printf("\nUSBIO Not Implemented yet .....\n");
+	exit(0);
+}
