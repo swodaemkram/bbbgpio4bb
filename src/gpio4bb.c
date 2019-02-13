6905bbe05eb2535156ca8966c624d2ca1c449564
mark <mark@mark@kyin.org>
2019-02-13 13:46:50 -0500

    Fixed Major Memory Leak Yeah !

--- src/gpio4bb.c
+++ src/gpio4bb.c
@@ -36,8 +36,10 @@ int Port_Out_To_BlackBox = 0;
 int Port_IN_From_BlackBox = 0;
 char *New_IO_Status_Value = {0};
 
-char Last_IO_Status_Value[37] = {0};
-char IO_Status_Value[37] = {0};
+//char Last_IO_Status_Value[37] = "0:0:0:0:0:0:0:0|00:00:00:00:00:00:00|";
+char Last_IO_Status_Value[38] = {0};
+//char IO_Status_Value[37] = {0};
+char IO_Status_Value[38] = "0:0:0:0:0:0:0:0|00:00:00:00:00:00:00|";
 char HardwarePlatform[10] = {0};
 int Verbose = 0;
 
@@ -170,13 +172,13 @@ while(1){
 
 		if(strcmp(IO_Status_Value, Last_IO_Status_Value) != 0){
 
-		// Deubug Code printf("\nLast_IO_Status_Value = %s\nNew_IO_Status_Value = %s\n",Last_IO_Status_Value,New_IO_Status_Value);
+		//printf("\nLast_IO_Status_Value = %s\n New_IO_Status_Value = %s\n",Last_IO_Status_Value,New_IO_Status_Value);
 
 			Send_Data_To_BlackBox(IP_Out_To_BlackBox, Port_Out_To_BlackBox, IO_Status_Value,  Verbose); //Send New Data To BlackBox
 
 		//Last_IO_Status_Value[0] ='\0';
 
-		strncpy(Last_IO_Status_Value,  IO_Status_Value, 38);
+		strncpy(Last_IO_Status_Value,  IO_Status_Value, 37);
 
 	}
 
@@ -190,20 +192,21 @@ while(1){
 //=========================================================================================================================
 	dif = strcmp(HardwarePlatform, "raspberrypi");            //are we raspberrypi ?
 		if (dif == 0){								          //are we raspberrypi ?
-		New_IO_Status_Value = RaspberryPi_Get_IO_Status(); //are we raspberrypi ?	get I/O from raspberrypi
+		New_IO_Status_Value = RaspberryPi_Get_IO_Status();    //are we raspberrypi ?	get I/O from raspberrypi
 		}											          //are we raspberrypi ?
 //=========================================================================================================================
 //=========================================================================================================================
 		dif = strcmp(HardwarePlatform, "usbio");          			  //are we usbio ?
 				if (dif == 0){								          //are we usbio ?
-				New_IO_Status_Value = USB_IO_Get_IO_Status(); //are we usbio ?	get I/O from usbio
+				New_IO_Status_Value = USB_IO_Get_IO_Status();         //are we usbio ?	get I/O from usbio
 				}											          //are we usbio ?
 //=========================================================================================================================
 
 // DEBUG !!! printf("\nIO_Status_Value = %s New_IO_Status_Value = %s\n", IO_Status_Value, New_IO_Status_Value);
 
 
-		strncpy(IO_Status_Value, New_IO_Status_Value,38);
+		strncpy(IO_Status_Value, New_IO_Status_Value,37);
+
 
 	usleep(500000);								             //This is set to .5 Seconds to keep the CPU usage to a minimum
 
