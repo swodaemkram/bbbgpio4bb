643d60a85631888e2dd15cc3fc4c47da14d58dbf
mark <mark@mark@kyin.org>
2019-02-13 09:58:12 -0500

    More USBIO Implementation

--- src/gpio4bb.c
+++ src/gpio4bb.c
@@ -22,6 +22,10 @@ Description	:	Service to provide GPIO In and Out capability to BlackBox on the V
 #include "RaspberryPi_Hardware_Initialize.h"
 #include "RaspberryPi_Get_IO_Status.h"
 
+#include "USB_IO_Initialize.h"
+#include "USB_IO_Get_IO_Status.h"
+
+
 #include "Print_Help.h"
 #include "Send_Data_To_BlackBox.h"
 #include "RX_Data_From_BlackBox.h"
@@ -116,11 +120,15 @@ Simple Argument Verification
 
  dif =  strcmp(HardwarePlatform, "beaglebone");
  	 if (dif != 0){
- 		 dif = strcmp(HardwarePlatform, "raspberrypi");
- 		 if (dif != 0){
- 			 printf("\nThe %s platform is not supported yet ....\n",HardwarePlatform);
- 			 Print_Help();
- 		 }
+ 		 	 dif = strcmp(HardwarePlatform, "raspberrypi");
+ 		 	 	 if (dif != 0){
+ 		 	 		 	 dif = strcmp(HardwarePlatform, "usbio");
+ 		 	 	 }
+
+ 		 	 	 	 if (dif != 0){
+ 		 	 	 		 printf("\nThe %s platform is not supported yet ....\n",HardwarePlatform);
+ 		 	 	 		 Print_Help();
+ 			 }
  	 }
 
 /*
@@ -130,17 +138,23 @@ Simple Argument Verification Completed
  */
 
 //====================================================================================================================
-	//int dif;								                //are we beagelbone ?
-dif = strcmp(HardwarePlatform, "beaglebone");               //are we beagelbone ?
-if (dif == 0){								                //are we beagelbone ?
-	BeagelBone_Hardware_Initialize();		                //are we beagelbone ?
-}											                //are we beagelbone ?
+	//int dif;								                //are we beaglebone ?
+dif = strcmp(HardwarePlatform, "beaglebone");               //are we beaglebone ?
+if (dif == 0){								                //are we beaglebone ?
+	BeagelBone_Hardware_Initialize();		                //are we beaglebone ?
+}											                //are we beaglebone ?
 //====================================================================================================================
  	 dif = strcmp(HardwarePlatform, "raspberrypi");         //are we RaspberryPi ?
  if (dif == 0){										        //are we RaspberriPi ?
  		 RaspberryPi_Hardware_Initialize();			        //are we RaspberryPi ?
- 		  }
+ 		  }													//are we RaspberryPi ?
 //====================================================================================================================
+ 	 dif = strcmp(HardwarePlatform, "usbio");				//are we USBIO ?
+ if (dif == 0){												//are we USBIO ?
+	 	 USB_IO_Initialize();								//are we USBIO ?
+ }															//are we USBIO ?
+//=====================================================================================================================
+
 
 printf("\nIP_Out_To_BlackBox = %s\n", IP_Out_To_BlackBox);
 printf("Port_Out_To_BlackBox = %d\n", Port_Out_To_BlackBox );
@@ -167,7 +181,6 @@ while(1){
 
 	}
 
-
 //=========================================================================================================================
 	dif = strcmp(HardwarePlatform, "beaglebone");             //are we beaglebone ?
 	if (dif == 0){								              //are we beaglebone ?
@@ -180,6 +193,12 @@ while(1){
 		if (dif == 0){								          //are we raspberrypi ?
 		   New_IO_Status_Value = RaspberryPi_Get_IO_Status(); //are we raspberrypi ?	get I/O from raspberrypi
 		}											          //are we raspberrypi ?
+//=========================================================================================================================
+//=========================================================================================================================
+		dif = strcmp(HardwarePlatform, "usbio");          			  //are we usbio ?
+				if (dif == 0){								          //are we usbio ?
+				   New_IO_Status_Value = USB_IO_Get_IO_Status(); //are we usbio ?	get I/O from usbio
+				}											          //are we usbio ?
 //=========================================================================================================================
 
 // DEBUG !!! printf("\nIO_Status_Value = %s New_IO_Status_Value = %s\n", IO_Status_Value, New_IO_Status_Value);
