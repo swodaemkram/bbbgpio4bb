dc3ac649748215101dbc79e01d2e2e25119b9062
mark <mark@mark@kyin.org>
2019-03-04 12:31:10 -0500

    test

--- src/bbb_dht_read.c
+++ src/bbb_dht_read.c
@@ -8,7 +8,7 @@
 // This is the only processor specific magic value, the maximum amount of time to
 // spin in a loop before bailing out and considering the read a timeout.  This should
 // be a high value, but if you're running on a much faster platform than a Raspberry
-// Pi or Beaglebone Black then it might need to be increased.
+// Pi or Beaglebone Black then it might need to be increased!
 #define DHT_MAXCOUNT 32000
 
 // Number of bit pulses to expect from the DHT.  Note that this is 41 because
